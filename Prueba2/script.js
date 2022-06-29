4b84fc0f82ea328b3c661f98078d0369528638b9
vicentemunozenr <101121746+vicentemunozenr@users.noreply.github.com>
2022-06-29 18:37:50 -0400

    Add files via upload

---
+++ Prueba2/script.js
@@ -0,0 +1,66 @@
+const datos = [
+    {
+        picsum: "img/expo1.webp",
+        title: "Casa Galería",
+        text: "Intervención de mi propio hogar, 2018",
+    },
+    {
+        picsum: "img/expo2.webp",
+        title: "Exposición de los estudiantes",
+        text: "En el 2017, los estudiantes tuvieron la oportunidad de colar sus trabajo de ese año en el colegio Epullay.",
+    },
+    {
+        picsum: "img/expo3.webp",
+        title: "Centro Lyon",
+        text: "Luego del exito de la Casa Galería, en el 2019 fui invitada a formar parte del Centro Lyon",
+    },
+];
+datos.forEach((d, i) => {
+    if (i % 2 == 0) {
+        // par
+        document.getElementById("trabajos").innerHTML +=
+            '<div class="row g-0"><div class="col-md-4 order-md-2 text-white showcase-img" style="background-image: url(' +
+            d.picsum +
+            ')"></div><div class="col-md-4 offset-md-2 order-md-1 my-auto showcase-text"><h2>' +
+            d.title +
+            '</h2><p class="lead mb-0">' +
+            d.text +
+            "</p></div></div>";
+    } else {
+        // impar
+        document.querySelector("#trabajos").innerHTML +=
+            '<div class="row g-0"><div class="col-md-4 offset-md-2 text-white showcase-img" style="background-image: url(' +
+            d.picsum +
+            ')"></div><div class="col-md-4 my-auto showcase-text"><h2>' +
+            d.title +
+            '</h2><p class="lead mb-0">' +
+            d.text +
+            "</p></div></div>";
+    }
+});
+
+users().catch((error) => console.error(error));
+
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
